9aa4b1d53a0c0c9d9aaff34e573bb7ea3b29515c
Maggie Bartlett <badjubies@Maggies-MacBook-Pro.local>
2024-02-05 21:42:17 -0500

    Created quote app

---
+++ index.js
@@ -0,0 +1,8 @@
+const quotations = [
+  "The love of money is the root of all evil.",
+  "I think therefore I am.",
+  "Knowledge is power",
+];
+
+const randomIndex = Math.floor(Math.random() * quotations.length);
+console.log(quotations[randomIndex]);
